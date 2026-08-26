773cbfc7b6fe0dc2b633500903f8986e00743f99
saadsharada65 <saadsharada65@gmail.com>
2026-08-26 10:46:58 +0300

    fix: overtime request

---
+++ hrms/hr/doctype/overtime_request/test_overtime_request.py
@@ -0,0 +1,14 @@
+# Copyright (c) 2026, Frappe Technologies Pvt. Ltd. and Contributors
+# See license.txt
+
+import frappe
+from frappe.tests import UnitTestCase
+
+
+class TestOvertimeRequest(UnitTestCase):
+	"""
+	Unit tests for OvertimeRequest.
+	Use this class for testing individual functions and methods.
+	"""
+
+	pass
